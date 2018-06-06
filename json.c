3d0cc724876d7819267faa1a9386d822d73737d5
Ryan M. Lederman <lederman@gmail.com>
2018-06-06 09:48:49 -0600

    tab indentation & permissions

--- json.c
+++ json.c
@@ -469,7 +469,7 @@ static int decode_unicode_char(json_parser *parser)
 	int offset = parser->buffer_offset;
 
 	uval = (hex(b[offset - 4]) << 12) | (hex(b[offset - 3]) << 8)
-	     | (hex(b[offset - 2]) << 4) | hex(b[offset - 1]);
+		 | (hex(b[offset - 2]) << 4) | hex(b[offset - 1]);
 
 	parser->buffer_offset -= 4;
 
@@ -667,7 +667,7 @@ static int do_action(json_parser *parser, int next_state)
  * return JSON_ERROR_NO_MEMORY if memory allocation failed or SUCCESS.
  */
 int json_parser_init(json_parser *parser, json_config *config,
-                     json_parser_callback callback, void *userdata)
+					 json_parser_callback callback, void *userdata)
 {
 	memset(parser, 0, sizeof(*parser));
 
@@ -729,7 +729,7 @@ int json_parser_is_done(json_parser *parser)
  * the user can supplied a valid processed pointer that will
  * be fill with the number of processed characters before returning */
 int json_parser_string(json_parser *parser, const char *s,
-                       uint32_t length, uint32_t *processed)
+					   uint32_t length, uint32_t *processed)
 {
 	int ret;
 	int next_class, next_state;
@@ -936,8 +936,8 @@ int json_print_raw(json_printer *printer, int type, const char *data, uint32_t l
 
 /** json_print_args takes multiple types and pass them to the printer function */
 int json_print_args(json_printer *printer,
-                    int (*f)(json_printer *, int, const char *, uint32_t),
-                    ...)
+					int (*f)(json_printer *, int, const char *, uint32_t),
+					...)
 {
 	va_list ap;
 	char *data;
@@ -978,44 +978,44 @@ int json_print_args(json_printer *printer,
 #ifndef JSON_NO_ERRORMSG
 const char* json_strerror(int err)
 {
-    switch (err)
-    {
-        case JSON_SUCCESS:
-            return "the operation completed successfully";
-        case JSON_ERROR_NO_MEMORY:
-            return "out of memory";
-        case JSON_ERROR_BAD_CHAR:
-            return "non-printable character encountered";
-        case JSON_ERROR_POP_EMPTY:
-            return "stack is empty; can't pop";
-        case JSON_ERROR_POP_UNEXPECTED_MODE:
-            return "bad type requested for stack pop";
-        case JSON_ERROR_NESTING_LIMIT:
-            return "stack nesting limit reached";
-        case JSON_ERROR_DATA_LIMIT:
-            return "buffer is full";
-        case JSON_ERROR_COMMENT_NOT_ALLOWED:
-            return "comments not supported by the configuration";
-        case JSON_ERROR_UNEXPECTED_CHAR:
-            return "unexpected character encountered";
-        case JSON_ERROR_UNICODE_MISSING_LOW_SURROGATE:
-            return "UTF low surrogate missing after high surrogate";
-        case JSON_ERROR_UNICODE_UNEXPECTED_LOW_SURROGATE:
-            return "unexpected UTF low surrogate without preceding high surrogate";
-        case JSON_ERROR_COMMA_OUT_OF_STRUCTURE:
-            return "unexpected comma encountered";
-        case JSON_ERROR_CALLBACK:
-            return "callback returned an error code";
-        case JSON_ERROR_UTF8:
-            return "invalid UTF stream encountered";
-        default:
-            return "<unknown>";
-    }
+	switch (err)
+	{
+		case JSON_SUCCESS:
+			return "the operation completed successfully";
+		case JSON_ERROR_NO_MEMORY:
+			return "out of memory";
+		case JSON_ERROR_BAD_CHAR:
+			return "non-printable character encountered";
+		case JSON_ERROR_POP_EMPTY:
+			return "stack is empty; can't pop";
+		case JSON_ERROR_POP_UNEXPECTED_MODE:
+			return "bad type requested for stack pop";
+		case JSON_ERROR_NESTING_LIMIT:
+			return "stack nesting limit reached";
+		case JSON_ERROR_DATA_LIMIT:
+			return "buffer is full";
+		case JSON_ERROR_COMMENT_NOT_ALLOWED:
+			return "comments not supported by the configuration";
+		case JSON_ERROR_UNEXPECTED_CHAR:
+			return "unexpected character encountered";
+		case JSON_ERROR_UNICODE_MISSING_LOW_SURROGATE:
+			return "UTF low surrogate missing after high surrogate";
+		case JSON_ERROR_UNICODE_UNEXPECTED_LOW_SURROGATE:
+			return "unexpected UTF low surrogate without preceding high surrogate";
+		case JSON_ERROR_COMMA_OUT_OF_STRUCTURE:
+			return "unexpected comma encountered";
+		case JSON_ERROR_CALLBACK:
+			return "callback returned an error code";
+		case JSON_ERROR_UTF8:
+			return "invalid UTF stream encountered";
+		default:
+			return "<unknown>";
+	}
 }
 
 void json_perror(int err)
 {
-    fputs(json_strerror(err), stderr);
+	fputs(json_strerror(err), stderr);
 }
 #endif /* !JSON_NO_ERRORMSG */
 
@@ -1045,9 +1045,9 @@ static int dom_pop(struct json_parser_dom *ctx, void **val)
 }
 
 int json_parser_dom_init(json_parser_dom *dom,
-                         json_parser_dom_create_structure create_structure,
-                         json_parser_dom_create_data create_data,
-                         json_parser_dom_append append)
+						 json_parser_dom_create_structure create_structure,
+						 json_parser_dom_create_data create_data,
+						 json_parser_dom_append append)
 {
 	memset(dom, 0, sizeof(*dom));
 	dom->stack_size = 1024;
